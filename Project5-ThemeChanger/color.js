124757947e06b8752a9d26011f57283940c2f05b
R.S.K.Abbas <rskabbas@outlook.com>
2021-08-20 13:03:53 +0530

    Add files via upload

---
+++ Project5-ThemeChanger/color.js
@@ -0,0 +1,80 @@
+document.getElementById('c1').onclick = switchC1;
+document.getElementById('c2').onclick = switchC2;
+document.getElementById('c3').onclick = switchC3;
+document.getElementById('c4').onclick = switchC4;
+document.getElementById('c5').onclick = switchC5;
+document.getElementById('c6').onclick = switchC6;
+document.getElementById('c7').onclick = switchC7;
+document.getElementById('c8').onclick = switchC8;
+document.getElementById('c9').onclick = switchC9;
+document.getElementById('c10').onclick = switchC10;
+
+
+//code for changing the colour found at w3schools
+function switchC1() {
+    //to fill the back ground color with the gradient color;
+    document.body.style.backgroundImage = 'linear-gradient(60deg, #96deda 0%, #50c9c3 100%)';
+
+    document.body.style.color = "red";
+}
+
+function switchC1() {
+    //to fill the back ground color with the gradient color;
+    document.body.style.backgroundImage = 'linear-gradient(90deg, #96deda 0%, #50c9c3 100%)';
+
+    document.body.style.color = "black";
+}
+function switchC2() {
+    //to fill the back ground color with the gradient color;
+    document.body.style.backgroundImage = 'linear-gradient(90deg, #a18cd1 0%, #fbc2eb 100%)';
+
+    document.body.style.color = "black";
+}
+function switchC3() {
+    //to fill the back ground color with the gradient color;
+    document.body.style.backgroundImage = 'linear-gradient(90deg, #fdcbf1 0%, #fdcbf1 1%, #e6dee9 100%)';
+
+    document.body.style.color = "black";
+}
+function switchC4() {
+    //to fill the back ground color with the gradient color;
+    document.body.style.backgroundImage = 'linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%)';
+
+    document.body.style.color = "black";
+}
+function switchC5() {
+    //to fill the back ground color with the gradient color;
+    document.body.style.backgroundImage = 'linear-gradient(to right, #d7d2cc 0%, #304352 100%)';
+
+    document.body.style.color = "black";
+}
+function switchC6() {
+    //to fill the back ground color with the gradient color;
+    document.body.style.backgroundImage = 'linear-gradient(to right, #4facfe 0%, #00f2fe 100%)';
+
+    document.body.style.color = "black";
+}
+function switchC7() {
+    //to fill the back ground color with the gradient color;
+    document.body.style.backgroundImage = 'linear-gradient(120deg, #a8edea 0%, #fed6e3 100%)';
+
+    document.body.style.color = "black";
+}
+function switchC8() {
+    //to fill the back ground color with the gradient color;
+    document.body.style.backgroundImage = 'linear-gradient(to right, #e4afcb 0%, #b8cbb8 0%, #b8cbb8 0%, #e2c58b 30%, #c2ce9c 64%, #7edbdc 100%)';
+
+    document.body.style.color = "black";
+}
+function switchC9() {
+    //to fill the back ground color with the gradient color;
+    document.body.style.backgroundImage = 'linear-gradient(to right, #eea2a2 0%, #bbc1bf 19%, #57c6e1 42%, #b49fda 79%, #7ac5d8 100%)';
+
+    document.body.style.color = "black";
+}
+function switchC10() {
+    //to fill the back ground color with the gradient color;
+    document.body.style.backgroundImage = 'linear-gradient(90deg, #3f51b1 0%, #5a55ae 13%, #7b5fac 25%, #8f6aae 38%, #a86aa4 50%, #cc6b8e 62%, #f18271 75%, #f3a469 87%, #f7c978 100%)';
+
+    document.body.style.color = "black";
+}
